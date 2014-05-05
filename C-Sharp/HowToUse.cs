d2c7fef90fa1f6964c6095eaece730b3601213fc
navneet83 <navneet.bits@gmail.com>
2014-05-05 10:22:28 -0700

    Update HowToUse.cs

--- C-Sharp/HowToUse.cs
+++ C-Sharp/HowToUse.cs
@@ -3,8 +3,8 @@
 		{
 			CryptLib _crypt = new CryptLib ();
 			string plainText = "This is the text to be encrypted";
-			String iv = CryptLib.GenerateRandomIV (15); //do not exceed 15
-			string key = CryptLib.getHashSha256("my secret key", 31); //do not exceed 31
+			String iv = CryptLib.GenerateRandomIV (16); //16 bytes = 128 bits
+			string key = CryptLib.getHashSha256("my secret key", 31); //32 bytes = 256 bits
 			String cypherText = _crypt.encrypt (plainText, key, iv);
 			Console.WriteLine ("iv="+iv);
 			Console.WriteLine ("key=" + key);
