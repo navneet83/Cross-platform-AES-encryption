6a38aa40523c5b1530d4bc3e625a3929ec246829
navneet83 <navneet.bits@gmail.com>
2014-05-05 02:30:11 -0700

    Update CryptLib.m

--- CryptLib.m
+++ CryptLib.m
@@ -1,26 +1,28 @@
 // How to use :
 // //  1. Encryption:
 
-//	NSString * _secret = @"This the sample text has to be encrypted"; // this is the text that you want to encrypt.
+// NSString * _secret = @"This the sample text has to be encrypted"; // this is the text that you want to encrypt.
 
-//	NSString * _key = @"shared secret"; //secret key for encryption. To make encryption stronger, we will not use this key directly. We'll first hash the key next step and then use it.
+// NSString * _key = @"shared secret"; //secret key for encryption. To make encryption stronger, we will not use this key directly. We'll first hash the key next step and then use it.
 
-//  key = [[StringEncryption alloc] sha256:key length:31]; //this is very important, do not exceed length > 31
+// key = [[StringEncryption alloc] sha256:key length:31]; //this is very important, do not exceed length > 31
 
-//  NSString * iv =   [[[[StringEncryption alloc] generateRandomIV:11]  base64EncodingWithLineLength:0] substringToIndex:15]; //Here we are generating random initialization vector (iv). Length of this vector should not exceed 15 characters. 
+// NSString * iv =   [[[[StringEncryption alloc] generateRandomIV:11]  base64EncodingWithLineLength:0] substringToIndex:15]; //Here we are generating random initialization vector (iv). Length of this vector should not exceed 15 characters. 
 
-//Now that we have input text, hashed key and random IV, we are all set for encryption:
-//NSData * encryptedData = [[StringEncryption alloc] encrypt:[secret dataUsingEncoding:NSUTF8StringEncoding] key:key iv:iv];
-//NSLog(@"encrypted data:: %@", [encryptedData  base64EncodingWithLineLength:0]); //print the encrypted text
+// Now that we have input text, hashed key and random IV, we are all set for encryption:
+// NSData * encryptedData = [[StringEncryption alloc] encrypt:[secret dataUsingEncoding:NSUTF8StringEncoding] key:key iv:iv];
+
+// NSLog(@"encrypted data:: %@", [encryptedData  base64EncodingWithLineLength:0]); //print the encrypted text
 // Encryption = [plainText + secretKey + randomIV] = Cyphertext
 
 // // 2. Decryption
 // for decryption, you will have to use the same IV and key which was used for encryption.
-//encryptedData = [[StringEncryption alloc] decrypt:encryptedData  key:key iv:iv];
- //NSString * decryptedText = [[NSString alloc] initWithData:encryptedData encoding:NSUTF8StringEncoding];
- //NSLog(@"decrypted data:: %@", decryptedText); //print the decrypted text
 
-//    For base64EncodingWithLineLength refer - https://github.com/jdg/MGTwitterEngine/blob/master/NSData%2BBase64.m
+// encryptedData = [[StringEncryption alloc] decrypt:encryptedData  key:key iv:iv];
+// NSString * decryptedText = [[NSString alloc] initWithData:encryptedData encoding:NSUTF8StringEncoding];
+// NSLog(@"decrypted data:: %@", decryptedText); //print the decrypted text
+
+// For base64EncodingWithLineLength refer - https://github.com/jdg/MGTwitterEngine/blob/master/NSData%2BBase64.m
 
 
 #import "StringEncryption.h"
