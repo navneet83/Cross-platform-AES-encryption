066f3851bb428e41cffae255d67a8c69448edbcf
navneet83 <navneet.bits@gmail.com>
2014-05-05 02:29:08 -0700

    Update CryptLib.m

--- CryptLib.m
+++ CryptLib.m
@@ -1,70 +1,187 @@
-//	NSString * _secret = @"This the sample text has to be encrypted";
-//	NSString * _key = @"shared secret";
-//
-//	//[contentView setText:str];
-//
-//    //encryption
-//    NSData * _encryptedData = [[StringEncryption alloc] encrypt:[_secret dataUsingEncoding:NSUTF8StringEncoding] key:_key];
-//    NSLog(@"encrypted data:: %@", [_encryptedData  base64EncodingWithLineLength:0]);
-//
-//    //decryption
-//    _encryptedData = [[StringEncryption alloc] decrypt:_encryptedData  key:_key];
-//    NSString * _decryptedText = [[NSString alloc] initWithData:_encryptedData encoding:NSUTF8StringEncoding];
-//    NSLog(@"decrypted data:: %@", _decryptedText);
+// How to use :
+// //  1. Encryption:
+
+//	NSString * _secret = @"This the sample text has to be encrypted"; // this is the text that you want to encrypt.
+
+//	NSString * _key = @"shared secret"; //secret key for encryption. To make encryption stronger, we will not use this key directly. We'll first hash the key next step and then use it.
+
+//  key = [[StringEncryption alloc] sha256:key length:31]; //this is very important, do not exceed length > 31
+
+//  NSString * iv =   [[[[StringEncryption alloc] generateRandomIV:11]  base64EncodingWithLineLength:0] substringToIndex:15]; //Here we are generating random initialization vector (iv). Length of this vector should not exceed 15 characters. 
+
+//Now that we have input text, hashed key and random IV, we are all set for encryption:
+//NSData * encryptedData = [[StringEncryption alloc] encrypt:[secret dataUsingEncoding:NSUTF8StringEncoding] key:key iv:iv];
+//NSLog(@"encrypted data:: %@", [encryptedData  base64EncodingWithLineLength:0]); //print the encrypted text
+// Encryption = [plainText + secretKey + randomIV] = Cyphertext
+
+// // 2. Decryption
+// for decryption, you will have to use the same IV and key which was used for encryption.
+//encryptedData = [[StringEncryption alloc] decrypt:encryptedData  key:key iv:iv];
+ //NSString * decryptedText = [[NSString alloc] initWithData:encryptedData encoding:NSUTF8StringEncoding];
+ //NSLog(@"decrypted data:: %@", decryptedText); //print the decrypted text
+
 //    For base64EncodingWithLineLength refer - https://github.com/jdg/MGTwitterEngine/blob/master/NSData%2BBase64.m
-//    NOTE -  This code is not yet ready for production use. IV and MD5 has be generated using a hash to make this more secure.
 
 
-- (NSData *)encrypt:(NSData *)_plainText key:(NSString *)_key {
-    char _keyPointer[kCCKeySizeAES128];
-    bzero(_keyPointer, sizeof(_keyPointer));
+#import "StringEncryption.h"
+#import "NSData+Base64.h"
+
+
+@implementation StringEncryption
+
+- (NSData *)encrypt:(NSData *)plainText key:(NSString *)key  iv:(NSString *)iv {
+    char keyPointer[kCCKeySizeAES256+1],// room for terminator (unused) ref: https://devforums.apple.com/message/876053#876053
+    ivPointer[kCCBlockSizeAES128];
+    BOOL patchNeeded;
+    bzero(keyPointer, sizeof(keyPointer)); // fill with zeroes for padding
+    //key = [[StringEncryption alloc] md5:key];
+    patchNeeded= ([key length] > kCCKeySizeAES256+1);
+    if(patchNeeded)
+    {
+        NSLog(@"Key length is longer %lu", (unsigned long)[[[StringEncryption alloc] md5:key] length]);
+        key = [key substringToIndex:kCCKeySizeAES256]; // Ensure that the key isn't longer than what's needed (kCCKeySizeAES256)
+    }
     
-    [_key getCString:_keyPointer maxLength:sizeof(_keyPointer) encoding:NSUTF8StringEncoding];
+    //NSLog(@"md5 :%@", key);
+    [key getCString:keyPointer maxLength:sizeof(keyPointer) encoding:NSUTF8StringEncoding];
+    [iv getCString:ivPointer maxLength:sizeof(ivPointer) encoding:NSUTF8StringEncoding];
     
-    NSUInteger _dataLength = [_plainText length];
+    if (patchNeeded) {
+        keyPointer[0] = '\0';  // Previous iOS version than iOS7 set the first char to '\0' if the key was longer than kCCKeySizeAES256
+    }
     
-    size_t _buffSize = _dataLength + kCCBlockSizeAES128;
-    void *buff = malloc(_buffSize);
+    NSUInteger dataLength = [plainText length];
     
-    size_t _numBytesEncrypted = 0;
+    //see https://developer.apple.com/library/ios/documentation/System/Conceptual/ManPages_iPhoneOS/man3/CCryptorCreateFromData.3cc.html
+    // For block ciphers, the output size will always be less than or equal to the input size plus the size of one block.
+    size_t buffSize = dataLength + kCCBlockSizeAES128;
+    void *buff = malloc(buffSize);
     
-    CCCryptorStatus _status = CCCrypt(kCCEncrypt, kCCAlgorithmAES128, kCCOptionPKCS7Padding,
-                                          _keyPointer, kCCKeySizeAES128,
-                                          _keyPointer, //iv
-                                          [_plainText bytes], [_plainText length], //input
-                                          buff, _buffSize,//output
-                                          &_numBytesEncrypted);
-    if (_status == kCCSuccess) {
-        return [NSData dataWithBytesNoCopy:buff length:_numBytesEncrypted];
+    size_t numBytesEncrypted = 0;
+    //refer to http://www.opensource.apple.com/source/CommonCrypto/CommonCrypto-36064/CommonCrypto/CommonCryptor.h
+    //for details on this function
+    //Stateless, one-shot encrypt or decrypt operation.
+    CCCryptorStatus status = CCCrypt(kCCEncrypt, /* kCCEncrypt, etc. */
+                                     kCCAlgorithmAES128, /* kCCAlgorithmAES128, etc. */
+                                     kCCOptionPKCS7Padding, /* kCCOptionPKCS7Padding, etc. */
+                                     keyPointer, kCCKeySizeAES256, /* key and its length */
+                                     ivPointer, /* initialization vector - use random IV everytime */
+                                     [plainText bytes], [plainText length], /* input  */
+                                     buff, buffSize,/* data RETURNED here */
+                                     &numBytesEncrypted);
+    if (status == kCCSuccess) {
+        return [NSData dataWithBytesNoCopy:buff length:numBytesEncrypted];
     }
     
     free(buff);
     return nil;
 }
 
--(NSData *)decrypt:(NSData *)_encryptedText key:(NSString *)_key {
-    char _keyPointer[kCCKeySizeAES128];
-    //bzero(_keyPointer, sizeof(_keyPointer));
+
+-(NSData *)decrypt:(NSData *)encryptedText key:(NSString *)key iv:(NSString *)iv {
+    char keyPointer[kCCKeySizeAES256+1],// room for terminator (unused) ref: https://devforums.apple.com/message/876053#876053
+    ivPointer[kCCBlockSizeAES128];
+    BOOL patchNeeded;
+  
+    patchNeeded = ([key length] > kCCKeySizeAES256+1);
+    if(patchNeeded)
+    {
+        NSLog(@"Key length is longer %lu", (unsigned long)[[[StringEncryption alloc] md5:key] length]);
+        key = [key substringToIndex:kCCKeySizeAES256]; // Ensure that the key isn't longer than what's needed (kCCKeySizeAES256)
+    }
+
+    [key getCString:keyPointer maxLength:sizeof(keyPointer) encoding:NSUTF8StringEncoding];
+    [iv getCString:ivPointer maxLength:sizeof(ivPointer) encoding:NSUTF8StringEncoding];
+
+    if (patchNeeded) {
+        keyPointer[0] = '\0';  // Previous iOS version than iOS7 set the first char to '\0' if the key was longer than kCCKeySizeAES256
+    }
     
-    [_key getCString:_keyPointer maxLength:sizeof(_keyPointer) encoding:NSUTF8StringEncoding];
+    NSUInteger dataLength = [encryptedText length];
     
-    NSUInteger _dataLength = [_encryptedText length];
+    //see https://developer.apple.com/library/ios/documentation/System/Conceptual/ManPages_iPhoneOS/man3/CCryptorCreateFromData.3cc.html
+    // For block ciphers, the output size will always be less than or equal to the input size plus the size of one block.
+    size_t buffSize = dataLength + kCCBlockSizeAES128;
     
-    size_t _buffSize = _dataLength + kCCBlockSizeAES128;
-    void *buff = malloc(_buffSize);
+    void *buff = malloc(buffSize);
     
-    size_t _numBytesEncrypted = 0;
-    
-    CCCryptorStatus _status = CCCrypt(kCCDecrypt, kCCAlgorithmAES128, kCCOptionPKCS7Padding,
-                                      _keyPointer, kCCKeySizeAES128,
-                                      _keyPointer, //iv
-                                      [_encryptedText bytes], [_encryptedText length], //input
-                                      buff, _buffSize,//output
-                                      &_numBytesEncrypted);
-    if (_status == kCCSuccess) {
-        return [NSData dataWithBytesNoCopy:buff length:_numBytesEncrypted];
+    size_t numBytesEncrypted = 0;
+    //refer to http://www.opensource.apple.com/source/CommonCrypto/CommonCrypto-36064/CommonCrypto/CommonCryptor.h
+    //for details on this function
+    //Stateless, one-shot encrypt or decrypt operation.
+    CCCryptorStatus status = CCCrypt(kCCDecrypt,/* kCCEncrypt, etc. */
+                                     kCCAlgorithmAES128, /* kCCAlgorithmAES128, etc. */
+                                     kCCOptionPKCS7Padding, /* kCCOptionPKCS7Padding, etc. */
+                                     keyPointer, kCCKeySizeAES256,/* key and its length */
+                                     ivPointer, /* initialization vector - use same IV which was used for decryption */
+                                     [encryptedText bytes], [encryptedText length], //input
+                                     buff, buffSize,//output
+                                     &numBytesEncrypted);
+    if (status == kCCSuccess) {
+        return [NSData dataWithBytesNoCopy:buff length:numBytesEncrypted];
     }
     
     free(buff);
     return nil;
 }
+
+//this function is no longer used in encryption / decryption
+- (NSString *) md5:(NSString *) input
+{
+    const char *cStr = [input UTF8String];
+    unsigned char digest[16];
+    CC_MD5( cStr, strlen(cStr), digest ); // This is the md5 call
+    
+    NSMutableString *output = [NSMutableString stringWithCapacity:CC_MD5_DIGEST_LENGTH * 2];
+    
+    for(int i = 0; i < CC_MD5_DIGEST_LENGTH; i++)
+        [output appendFormat:@"%02x", digest[i]];
+    
+    return  output;
+    
+}
+
+//function to generate random string of given length. 
+//random strings are used as IV
+- (NSData *)generateRandomIV:(size_t)length
+{
+    NSMutableData *data = [NSMutableData dataWithLength:length];
+    
+    int output = SecRandomCopyBytes(kSecRandomDefault,
+                                    length,
+                                    data.mutableBytes);
+    NSAssert(output == 0, @"error generating random bytes: %d",
+             errno);
+    
+    return data;
+}
+
+    /**
+	 * This function computes the SHA256 hash of input string
+	 * @param text input text whose SHA256 hash has to be computed
+	 * @param length length of the text to be returned
+	 * @return returns SHA256 hash of input text 
+	 */
+- (NSString*) sha256:(NSString *)key length:(NSInteger) length{
+    const char *s=[key cStringUsingEncoding:NSASCIIStringEncoding];
+    NSData *keyData=[NSData dataWithBytes:s length:strlen(s)];
+    
+    uint8_t digest[CC_SHA256_DIGEST_LENGTH]={0};
+    CC_SHA256(keyData.bytes, keyData.length, digest);
+    NSData *out=[NSData dataWithBytes:digest length:CC_SHA256_DIGEST_LENGTH];
+    NSString *hash=[out description];
+    hash = [hash stringByReplacingOccurrencesOfString:@" " withString:@""];
+    hash = [hash stringByReplacingOccurrencesOfString:@"<" withString:@""];
+    hash = [hash stringByReplacingOccurrencesOfString:@">" withString:@""];
+    
+    if(length > [hash length])
+    {
+        return  hash;
+    }
+    else
+    {
+        return [hash substringToIndex:length];
+    }
+ }
+
+@end
