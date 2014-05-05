39a4b433ff5ce5609742e9da3e23751d103915ea
navneet83 <navneet.bits@gmail.com>
2014-05-05 03:33:31 -0700

    Update CryptLib.cs

--- C-Sharp/CryptLib.cs
+++ C-Sharp/CryptLib.cs
@@ -8,11 +8,13 @@ namespace com.pakhee.common
 	 * CrossPlatform CryptLib
 	 * 
 	 * <p>
-	 * This is a very basic cross platform implementation of AES encryption which is
-	 * not yet ready for production use. Idea is to make encryption work across
-	 * Android, iOS and C# by using the same key/cipher mode/padding/IV on all platforms.<br/>
-	 * Security Issues: Key has to be salted (PBKDF2),
-	 * randomize IV <br/>
+	 * This cross platform CryptLib uses AES 256 for encryption. This library can
+	 * be used for encryptoion and de-cryption of string on iOS, Android and Windows
+	 * platform.<br/>
+	 * Features: <br/>
+	 * 1. 256 bit AES encryption
+	 * 2. Random IV generation. 
+	 * 3. Provision for SHA256 hashing of key. 
 	 * </p>
 	 * 
 	 * @since 1.0
@@ -22,12 +24,47 @@ namespace com.pakhee.common
 	{
 		UTF8Encoding _enc;
 		RijndaelManaged _rcipher;
-		byte[] _key, _pwd;
-
+		byte[] _key, _pwd, _ivBytes, _iv;
+		
 		/***
 		 * Encryption mode enumeration
 		 */
 		private enum EncryptMode {ENCRYPT, DECRYPT};
+		
+		static readonly char[] CharacterMatrixForRandomIVStringGeneration = {
+			'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 
+			'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 
+			'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 
+			'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 
+			'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '-', '_'
+		};
+		
+		/**
+		 * This function generates random string of the given input length.
+		 * 
+		 * @param _plainText
+		 *            Plain text to be encrypted
+		 * @param _key
+		 *            Encryption Key. You'll have to use the same key for decryption
+		 * @return returns encrypted (cipher) text
+		 */
+		internal static string GenerateRandomIV(int length) {
+			char[] _iv = new char[length];
+			byte[] randomBytes = new byte[length];
+
+			using (RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider()) {
+				rng.GetBytes(randomBytes);
+			}
+
+			for (int i = 0; i < _iv.Length; i++) {
+				int ptr = randomBytes[i] % CharacterMatrixForRandomIVStringGeneration.Length;
+				_iv[i] = CharacterMatrixForRandomIVStringGeneration[ptr];
+			}
+
+			return new string(_iv);
+		}
+		
+		
 
 		public CryptLib()
 		{
@@ -35,9 +72,11 @@ namespace com.pakhee.common
 			_rcipher =  new RijndaelManaged();
 			_rcipher.Mode = CipherMode.CBC;
 			_rcipher.Padding = PaddingMode.PKCS7;
-			_rcipher.KeySize = 0x80;
-			_rcipher.BlockSize = 0x80;
-			_key = new byte[0x10];
+			_rcipher.KeySize = 256;
+			_rcipher.BlockSize = 128;
+			_key = new byte[32];
+			_iv = new byte[_rcipher.BlockSize/8]; //128 bit / 8 = 16 bytes
+			_ivBytes =  new byte[16];
 		}
 
 		/**
@@ -48,27 +87,33 @@ namespace com.pakhee.common
 		 *            Encryption key to used for encryption / decryption
 		 * @param _mode
 		 *            specify the mode encryption / decryption
+		 * @param _initVector
+		 * 			  initialization vector
 		 * @return encrypted or decrypted string based on the mode
-		 * @throws UnsupportedEncodingException
-		 * @throws InvalidKeyException
-		 * @throws InvalidAlgorithmParameterException
-		 * @throws IllegalBlockSizeException
-		 * @throws BadPaddingException
 	 	*/
-		private string encryptDecrypt (string _inputText, string _encryptionKey, EncryptMode _mode)
+		private String encryptDecrypt (string _inputText, string _encryptionKey, EncryptMode _mode, string _initVector)
 		{
-			String _out = "";// output string
+
+			string _out = "";// output string
+			//_encryptionKey = MD5Hash (_encryptionKey);
 			_pwd = Encoding.UTF8.GetBytes(_encryptionKey);
+			_ivBytes = Encoding.UTF8.GetBytes (_initVector);
 
 			int len = _pwd.Length;
 			if (len > _key.Length)
 			{
 				len = _key.Length;
 			}
+			int ivLenth = _ivBytes.Length;
+			if (ivLenth > _iv.Length) 
+			{
+				ivLenth = _iv.Length;		
+			}
 
 			Array.Copy(_pwd, _key, len);
+			Array.Copy (_ivBytes, _iv, ivLenth);
 			_rcipher.Key = _key;
-			_rcipher.IV = _key;
+			_rcipher.IV = _iv;
 
 			if (_mode.Equals (EncryptMode.ENCRYPT)) {
 				//encrypt
@@ -84,8 +129,7 @@ namespace com.pakhee.common
 			return _out;// return encrypted/decrypted string
 		}
 
-
-		/***
+		/**
 		 * This function encrypts the plain text to cipher text using the key
 		 * provided. You'll have to use the same key for decryption
 		 * 
@@ -94,15 +138,10 @@ namespace com.pakhee.common
 		 * @param _key
 		 *            Encryption Key. You'll have to use the same key for decryption
 		 * @return returns encrypted (cipher) text
-		 * @throws InvalidKeyException
-		 * @throws UnsupportedEncodingException
-		 * @throws InvalidAlgorithmParameterException
-		 * @throws IllegalBlockSizeException
-		 * @throws BadPaddingException
 		 */
-		public string encrypt (string _plainText, string _key)
+		public string encrypt (string _plainText, string _key, string _initVector)
 		{
-			return encryptDecrypt(_plainText, _key, EncryptMode.ENCRYPT);
+			return encryptDecrypt(_plainText, _key, EncryptMode.ENCRYPT, _initVector);
 		}
 
 		/***
@@ -115,15 +154,59 @@ namespace com.pakhee.common
 		 * @param _key
 		 *            Encryption key which you used during encryption
 		 * @return encrypted value
-		 * @throws InvalidKeyException
-		 * @throws UnsupportedEncodingException
-		 * @throws InvalidAlgorithmParameterException
-		 * @throws IllegalBlockSizeException
-		 * @throws BadPaddingException
 		 */
-		public string decrypt(string _encryptedText, string _key)
+		 
+		public string decrypt(string _encryptedText, string _key, string _initVector)
 		{
-			return encryptDecrypt(_encryptedText, _key, EncryptMode.DECRYPT);
+			return encryptDecrypt(_encryptedText, _key, EncryptMode.DECRYPT, _initVector);
+		}
+
+		/***
+		 * This function decrypts the encrypted text to plain text using the key
+		 * provided. You'll have to use the same key which you used during
+		 * encryption
+		 * 
+		 * @param _encryptedText
+		 *            Encrypted/Cipher text to be decrypted
+		 * @param _key
+		 *            Encryption key which you used during encryption
+		 */
+		public static string getHashSha256(string text, int length)
+		{
+			byte[] bytes = Encoding.UTF8.GetBytes(text);
+			SHA256Managed hashstring = new SHA256Managed();
+			byte[] hash = hashstring.ComputeHash(bytes);
+			string hashString = string.Empty;
+			foreach (byte x in hash)
+			{
+				hashString += String.Format("{0:x2}", x); //covert to hex string
+			}
+			if (length > hashString.Length)
+				return hashString;
+			else
+				return hashString.Substring (0, length);
+		}
+		
+		//this function is no longer used.
+		private static string MD5Hash(string text)
+		{
+			MD5 md5 = new MD5CryptoServiceProvider();
+
+			//compute hash from the bytes of text
+			md5.ComputeHash(ASCIIEncoding.ASCII.GetBytes(text));
+
+			//get hash result after compute it
+			byte[] result = md5.Hash;
+
+			StringBuilder strBuilder = new StringBuilder();
+			for (int i = 0; i < result.Length; i++)
+			{
+				//change it into 2 hexadecimal digits
+				//for each byte
+				strBuilder.Append(result[i].ToString("x2"));
+			}
+			Console.WriteLine ("md5 hash of they key=" + strBuilder.ToString ());
+			return strBuilder.ToString();
 		}
 
 	}
