f8287520fd527f87196b2b920ca6586aa18f2b86
Erwan Lent <erwanlent@gmail.com>
2014-08-03 18:02:09 -0400

    Cleaned up logic. Made code actually readable.

--- C-Sharp/HowToUse.cs
+++ C-Sharp/HowToUse.cs
@@ -1,15 +1,15 @@
-	public class test {
-		public static void Main (String []args)
-		{
-			CryptLib _crypt = new CryptLib ();
-			string plainText = "This is the text to be encrypted";
-			String iv = CryptLib.GenerateRandomIV (16); //16 bytes = 128 bits
-			string key = CryptLib.getHashSha256("my secret key", 31); //32 bytes = 256 bits
-			String cypherText = _crypt.encrypt (plainText, key, iv);
-			Console.WriteLine ("iv="+iv);
-			Console.WriteLine ("key=" + key);
-			Console.WriteLine("Cypher text=" + cypherText);
-			Console.WriteLine ("Plain text =" + _crypt.decrypt (cypherText, key, iv));
-		}
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            CryptLib crypt = new CryptLib("password", "iv");
 
-	}
+            string encryptedText = crypt.Encrypt("this is a secret message");
+            Console.WriteLine(encryptedText);
+
+            string decryptedText = crypt.Decrypt(encryptedText);
+            Console.WriteLine(decryptedText);
+
+            Console.ReadLine();
+        }
+    }
